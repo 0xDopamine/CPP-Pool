1fe984c160c66fc04cc898a452d97c4d0ed78e2c
0xDopamine <aminebumi1@gmail.com>
2022-12-08 00:10:50 +0100

    finished Harl

--- C01/ex05/Harl.cpp
+++ C01/ex05/Harl.cpp
@@ -6,12 +6,14 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 12:38:40 by mbaioumy          #+#    #+#             */
-/*   Updated: 2022/12/06 23:27:38 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2022/12/08 00:09:55 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+#define	callMemberFunction(object,ptrToMember) ((object).*(ptrToMember))
+typedef void    (Harl::*pfMemFn) (void); 
 
 Harl::Harl() {};
 
@@ -19,5 +21,12 @@ Harl::~Harl() {};
 
 void    Harl::complain(std::string level)
 {
-    
+	Harl	harl;
+	pfMemFn arr[4] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+	std::string levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++) {
+		if (level == levels[i]) {
+			callMemberFunction(harl, arr[i]) ();
+		}
+	}
 }
--- C01/ex05/main.cpp
+++ C01/ex05/main.cpp
@@ -6,23 +6,18 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 19:15:21 by mbaioumy          #+#    #+#             */
-/*   Updated: 2022/12/06 23:19:22 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2022/12/08 00:09:40 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
-#include "Harl.cpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
     Harl harl;
-    harl.complain("DEBUG");
-    std::cout << std::endl;
-    harl.complain("INFO");
-    std::cout << std::endl;
-    harl.complain("WARNING");
-    std::cout << std::endl;
-    harl.complain("ERROR");
-    std::cout << std::endl;
+    std::string level = argv[1];
+    (void)argc;
+
+    harl.complain(level);
     return (0);
 }
