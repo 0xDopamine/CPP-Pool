9dbfbeeaa38c73fcd19026689ae0ca12e693a290
0xDopamine <aminebumi1@gmail.com>
2022-12-23 17:49:56 +0100

    started checking on the point if its a vertex or on edge

--- C02/ex03/Point.cpp
+++ C02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 22:16:41 by mbaioumy          #+#    #+#             */
-/*   Updated: 2022/12/22 20:36:13 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2022/12/23 17:46:09 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,68 @@ Fixed	Point::getY ( void ) const {
 	return y;
 }
 
+bool	vertex(Point const a, Point const b, Point const c, Point const point)
+{
+	if ((a.getX() == point.getX() && a.getY() == point.getY()) || 
+		(b.getX() == point.getX() && b.getY() == point.getY()) ||
+		(c.getX() == point.getX() && c.getY() == point.getY()))
+		return (true);
+	return (false);
+}
+
+bool	isCollinear(Point const point, Point const a, Point const b)
+{
+	return ((point.getY() - a.getY()) * (point.getX() - b.getX()) == (point.getY() - b.getY()) * (point.getX() - a.getX()));	
+}
+
+bool	isOnEdge(Point const a, Point const b, Point const c, Point const point)
+{
+	if (isCollinear(point, a, b))
+	{
+		if (point.getX() >= Fixed::min(a.getX(), b.getX()) &&
+			point.getX() <= Fixed::max(a.getX(), b.getX()) &&
+			point.getY() >= Fixed::min(a.getY(), b.getY()) &&
+			point.getY() <= Fixed::max(a.getY(), b.getY()))
+				return (true);
+	}
+	if (isCollinear(point, a, c))
+	{
+		if (point.getX() >= Fixed::min(a.getX(), c.getX()) &&
+			point.getX() <= Fixed::max(a.getX(), c.getX()) &&
+			point.getY() >= Fixed::min(a.getY(), c.getY()) &&
+			point.getY() <= Fixed::max(a.getY(), c.getY()))
+				return (true);
+	}
+	if (isCollinear(point, b, c))
+	{
+		if (point.getX() >= Fixed::min(b.getX(), c.getX()) &&
+			point.getX() <= Fixed::max(b.getX(), c.getX()) &&
+			point.getY() >= Fixed::min(b.getY(), c.getY()) &&
+			point.getY() <= Fixed::max(b.getY(), c.getY()))
+				return (true);
+	}
+	return (false);
+}
+
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	// area of ABC
 	Fixed	alpha;
-	// area of PAB
 	Fixed	bravo;
-	// area of PBC
 	Fixed	charlie;
-	// area of PAC
 	Fixed	echo;
 
-	alpha = ((a.getX() * (b.getY() - c.getY())) + (b.getX() * (c.getY() - a.getY())) + (c.getX() * (a.getY() - b.getY()))) / 2;
-	bravo = ((point.getX() * (a.getY() - b.getY())) + (a.getX() * (b.getY() - point.getY())) + (b.getX() * (point.getY() - a.getY()))) / 2;
-	charlie = ((point.getX() * (b.getY() - c.getY())) + (b.getX() * (c.getY() - point.getY())) + (c.getX() * (point.getY() - b.getY()))) / 2;
-	echo = ((point.getX() * (a.getY() - c.getY())) + (a.getX() * (c.getY() - point.getY())) + (c.getX() * (point.getY() - a.getY()))) / 2;
+	alpha = ((a.getX() * (b.getY() - c.getY())) + 
+			(b.getX() * (c.getY() - a.getY())) +
+			(c.getX() * (a.getY() - b.getY()))) / 2;
+	bravo = ((point.getX() * (a.getY() - b.getY())) +
+			(a.getX() * (b.getY() - point.getY())) +
+			(b.getX() * (point.getY() - a.getY()))) / 2;
+	charlie = ((point.getX() * (b.getY() - c.getY())) +
+			(b.getX() * (c.getY() - point.getY())) +
+			(c.getX() * (point.getY() - b.getY()))) / 2;
+	echo = ((point.getX() * (a.getY() - c.getY())) +
+			(a.getX() * (c.getY() - point.getY())) +
+			(c.getX() * (point.getY() - a.getY()))) / 2;
 
 	if (alpha < 0)
 		alpha = alpha * -1;
@@ -60,10 +107,10 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 		charlie = charlie * -1;
 	if (echo < 0)
 		echo = echo * -1;
-	
+
 	Fixed	testAlpha = bravo + charlie + echo;
 
-	if (testAlpha == alpha)
+	if (testAlpha == alpha && !vertex(a, b, c, point) && !isOnEdge(a, b, c, point))
 		return (true);
 	else
 		return (false);
--- C02/ex03/main.cpp
+++ C02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 01:41:56 by mbaioumy          #+#    #+#             */
-/*   Updated: 2022/12/22 20:37:19 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2022/12/23 17:46:40 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 
 int	main( void ) {
 
-	Point	a(-2, 4.12f);
-	Point	b(2.22f, 1);
-	Point	c(-3, -2.40f);
+	Point	a(2, 3);
+	Point	b(3, 1);
+	Point	c(1, 1);
 
-	Point	point(1, 5);
+	Point	point(2, 1);
 
 	if (bsp(a, b, c, point))
 		std::cout << "inside" << std::endl;
