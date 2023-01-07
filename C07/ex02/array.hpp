97904cc6fbaf7a7a422bbf7c77a4ef5d194c5e98
0xDopamine <aminebumi1@gmail.com>
2023-01-07 02:13:19 +0100

    init ex02 c07

--- C07/ex02/array.hpp
+++ C07/ex02/array.hpp
@@ -6,7 +6,7 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:55:06 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/01/05 17:56:33 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/01/07 02:11:02 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,31 @@ template <typename T>
 class _Array {
 
 	private:
-		
+		T				*array;
+		unsigned int	n;
 	public:
-		_Array();
-		~_Array();
-		int	length() const;
-		
+		_Array() {
+			new array;
+			std::cout << "array has been allocated" << std::endl;
+		};
+		_Array (unsigned int n) {
+
+			new array[n];
+			std::cout << "array has been allocated using size" << std::endl;
+		}
+		// _Array(const _Array& arr) {
+			
+		// };
+		~_Array() {
+			delete [] array;
+		};
+		int&	operator[](int index) {
+			if (index >= n)
+				std::cout << "index is out of bounds!" << std::endl;
+			return (array[index]);
+		};
+		size_t	length() const {
+			return (n);
+		};
+
 } ;
