8d33b670ef442a0db107df7a141ad7e41666d113
0xDopamine <aminebumi1@gmail.com>
2023-01-09 20:45:19 +0100

    useless ex02

--- C08/ex02/MutantStack.hpp
+++ C08/ex02/MutantStack.hpp
@@ -6,16 +6,19 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:58:51 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/01/09 19:11:47 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/01/09 19:51:57 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack {
+#include <iostream>
+#include <stack>
+
+class MutantStack: public std::stack {
 
     
-}
+} ;
 
 #endif
