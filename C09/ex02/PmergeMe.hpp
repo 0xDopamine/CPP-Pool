dea968a3d5d5ba9a09afd9e2665bfd9bddb26c52
0xDopamine <aminebumi1@gmail.com>
2023-04-05 21:24:26 +0000

    some progress on ex02, insertion sort done

--- C09/ex02/PmergeMe.hpp
+++ C09/ex02/PmergeMe.hpp
@@ -6,7 +6,16 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 20:27:39 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/04/04 20:27:40 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/04/05 20:52:22 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+#endif
--- C09/ex02/main.cpp
+++ C09/ex02/main.cpp
@@ -6,7 +6,35 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 20:27:46 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/04/04 20:27:47 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/04/05 21:05:21 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+void	insertion(std::vector<int>& vec)
+{
+	for (unsigned long i = 1; i < vec.size(); i++)
+	{
+		int	key = vec[i];
+		int	j = i - 1;
+
+		while (j >= 0 && vec[j] > key)
+		{
+			vec[j + 1] = vec[j];
+			j--;
+		}
+		vec[j + 1] = key;
+	}
+}
+
+int main(int argc, char **argv) {
+
+	std::vector<int>	vec;
+	
+	for (int i = 1; i < argc; i++)
+		vec.push_back(std::atoi(argv[i]));
+	insertion(vec);
+	for (std::vector<int>::iterator it = vec.begin(); it < vec.end(); it++)
+		std::cout << *it << std::endl;
+}
